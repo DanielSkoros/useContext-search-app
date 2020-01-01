a0ae17724c302bf3e5c5cdbe2c6e11a76f4c5d69
DanielSkoros <morsmorfie@gmail.com>
2020-01-01 13:56:49 +0100

    Updates

---
+++ precache-manifest.2b1add59e36f5c6446f19467c56217ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cce39c9eb3f11334d9c53d7f0fd85c4",
+    "url": "/useContext-search-app/index.html"
+  },
+  {
+    "revision": "cd343a9243e95f19e0cd",
+    "url": "/useContext-search-app/static/css/main.c27254cd.chunk.css"
+  },
+  {
+    "revision": "f5aa401a95c28c0faabf",
+    "url": "/useContext-search-app/static/js/2.aacf27c5.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/useContext-search-app/static/js/2.aacf27c5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd343a9243e95f19e0cd",
+    "url": "/useContext-search-app/static/js/main.be7bc2ee.chunk.js"
+  },
+  {
+    "revision": "17c9531c8a449c697b29",
+    "url": "/useContext-search-app/static/js/runtime-main.be9bcceb.js"
+  }
+]);
