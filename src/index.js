6a5d7ce6c76eab1e0fcf44d4f2cd47ddecbea688
DanielSkoros <morsmorfie@gmail.com>
2020-01-01 14:10:28 +0100

    deploy

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import Header from "./components/Header";
 import {MoviesProvider} from "./context/MovieContext";
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter basename='/'>
         <MoviesProvider>
             <Header />
             <App />
         </MoviesProvider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root'));
